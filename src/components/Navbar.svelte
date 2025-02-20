9a86475905725927d1522f378e72320ce2db5c0e
Anusree Purushothaman <anusreepurushothaman@MacBook-Pro.local>
2025-02-20 13:02:57 +0530

    changes

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,7 +2,7 @@
     import { onMount } from 'svelte';
      export let menu_open;
      let y = 0;
-    let pageHeight = 250;
+    let pageHeight = 1;
     let dropdownOpen = false; // Dropdown state
     let dropdownOpensm = false; // Dropdown state
   
@@ -56,31 +56,33 @@
   
   <svelte:window bind:scrollY={y}/>
   
-    <div id="navbar"  class="fixed space-x-4 shadow-md md:space-x-0 min-h-fit w-screen sm:min-h-0  left-0 top-0
+    <div id="navbar"  class="fixed space-x-4 shadow-md md:space-x-0 min-h-fit w-screen sm:min-h-0  left-0
    flex sm:items-center
     ease-in-out transition-all transform duration-300
     z-50  py-2
-    {scrolled?'bg-white':'bg-transparent'}
+           {scrolled ? 'bg-white top-0' : 'bg-transparent sm:top-14'}
+
+
    ">
     <div class="w-full container mx-auto  flex justify-between px-4">
      <div class=" md:px-2 m-0 font-medium gap-2 w-fit p-0 rounded-md flex items-center justify-center ">
      
         <a href="/" >
           <img
-          src="{scrolled ? '/img/logo.svg' : '/img/logo-white.svg'}"
+          src="{scrolled ? '/img/logo.svg' : '/img/logo.svg'}"
           alt="logo"
-          class="items-center justify-center md:h-14 h-12"
+          class="items-center justify-center  h-12"
         /></a>
         <a href="/" >
           <img
-          src="{scrolled ? '/img/logo1.svg' : '/img/logo11.svg'}"
+          src="{scrolled ? '/leap/leap.svg' : '/leap/leap.svg'}"
           alt="logo"
-          class="items-center justify-center md:h-14 h-12"
+          class="items-center justify-center h-12"
         /></a>
      </div>
   
      <main class=" flex  items-start px-7 my-auto">
-        <div style="font-family: Museo-Sans, sans-serif;" class="flex my-auto  {scrolled?'text-black':'text-white'}">
+        <div style="font-family: Museo-Sans, sans-serif;" class="flex my-auto  {scrolled?'text-black':'text-black'}">
             <a href="/"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">Home</a>
             <a href="/#conclave"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">MICROBIOME CONCLAVE</a>
             <a href="/#ideathon"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">MICROBIOME IDEATHON
@@ -93,10 +95,10 @@
       
         <div class="flex  items-center justify-center  bg-black my-auto spl_cursor md:hidden flex ">   
              <div on:click="{()=> menu_open = !menu_open }" class="z-50 items-center justify-center my-auto spl_cursor md:pt-4 pt-1  bg-black absolute cursor-pointer top-3 md:right-8 right-4 w-10 h-10 md:text-xl text-xs ml-auto transition duration-150 hover:shadow py-1  -solid -transparent rounded bg-transparent block outline-none focus:outline-none" type="button">
-                <div class="block absolute w-6 h-0.5 rounded-sm {scrolled?'bg-black':'bg-white'} top-5  duration-500 { menu_open ? 'opacity-0':'' }" ></div>
-                <div class="block absolute w-6 h-0.5 rounded-sm {scrolled?'bg-black':'bg-white'} top-6 duration-500 { menu_open ? 'transform rotate-45':'' }"></div>
-                <div class="block absolute w-6 h-0.5 rounded-sm {scrolled?'bg-black':'bg-white'} top-7 duration-500 { menu_open ? 'transform -rotate-45':'' }"></div>
-                <div class="block absolute w-6 h-0.5 rounded-sm {scrolled?'bg-black':'bg-white'} top-8 duration-500 { menu_open ? 'opacity-0':'' }" ></div>
+                <div class="block absolute w-6 h-0.5 rounded-sm bg-black top-5  duration-500 { menu_open ? 'opacity-0':'' }" ></div>
+                <div class="block absolute w-6 h-0.5 rounded-sm bg-black top-6 duration-500 { menu_open ? 'transform rotate-45':'' }"></div>
+                <div class="block absolute w-6 h-0.5 rounded-sm bg-black top-7 duration-500 { menu_open ? 'transform -rotate-45':'' }"></div>
+                <div class="block absolute w-6 h-0.5 rounded-sm bg-black top-8 duration-500 { menu_open ? 'opacity-0':'' }" ></div>
             </div>
         </div>
     </main>
@@ -123,7 +125,7 @@
     <div class="w-fit container mx-auto sm:items-center sm:justify-between">
         <div class="mb-8 gap-2  font-medium sm:bg-transparent p-5 sm:p-0 rounded-md flex items-center justify-center">
             <img src="/img/logo11.svg" alt="logo" class="md:h-14 h-12">
-            <img src="/img/logo-white.svg" alt="logo" class="md:h-14 h-12">
+            <img src="/img/leap.svg" alt="logo" class="md:h-14 h-12">
   
         </div>
          <nav>
