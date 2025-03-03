1bd7fbe9647d8607f6148de97dee048634dfe817
Anusree Purushothaman <anusreepurushothaman@MacBook-Pro.local>
2025-03-03 16:11:24 +0530

    changes

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -60,7 +60,7 @@
    flex sm:items-center
     ease-in-out transition-all transform duration-300
     z-50  py-2
-           {scrolled ? 'bg-white top-0' : 'bg-transparent sm:top-14'}
+           {scrolled ? 'bg-white top-0' : 'bg-white sm:top-14'}
 
 
    ">
@@ -84,11 +84,10 @@
      <main class=" flex  items-start px-7 my-auto">
         <div style="font-family: Museo-Sans, sans-serif;" class="flex my-auto  {scrolled?'text-black':'text-black'}">
             <a href="/"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">Home</a>
-            <a href="/#conclave"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">MICROBIOME CONCLAVE</a>
-            <a href="/#ideathon"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">MICROBIOME IDEATHON
-            </a>
+            <a href="/leap-co-works"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">LEAP CO-WORKS</a>
+            <a href="/leap-membership"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">LEAP MEMBERSHIP</a>
             
-            <!-- <a href="/#speakers"  class="md:flex hidden cursor-pointer  px-4 py-3  md:py-1 uppercase   text-sm">Speakers</a> -->
+            <a href="/#facility-booking"  class="md:flex hidden cursor-pointer  px-4 py-3  md:py-1 uppercase   text-sm">FACILITY BOOKING</a>
             <!-- <a href="/#LEAP_Membership"  class="md:flex cursor-pointer hidden px-4 py-3  md:py-1 uppercase   text-sm">Partners</a> -->
            
         </div>
@@ -131,17 +130,17 @@
          <nav>
             <ul class=" md:text-sm gap-2 font-data">
                     <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
-                        <a href="/#home"  class="cursor-pointer text-white font-bold hover:text-black uppercase ">Home</a>
+                        <a href="/"  class="cursor-pointer text-white font-bold hover:text-black uppercase ">Home</a>
                     </li>
                     <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
-                      <a href="/#events"  class="cursor-pointer text-white font-bold hover:text-black uppercase ">Events</a>
+                      <a href="/leap-co-works"  class="cursor-pointer text-white font-bold hover:text-black uppercase ">LEAP CO-WORKS</a>
                     </li>
-                    <!-- <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
-                      <a href="/#LEAP_Membership"  class="cursor-pointer text-white font-bold hover:text-black uppercase ">Partners</a>
-                    </li> -->
-                    <!-- <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
-                      <a href="/#speakers"  class="cursor-pointer text-white font-bold hover:text-black uppercase ">Speakers</a>
-                    </li> -->
+                    <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
+                      <a href="/#leap-membership"  class="cursor-pointer text-white font-bold hover:text-black uppercase ">LEAP MEMBERSHIP</a>
+                    </li> 
+                     <li on:click="{()=> menu_open = !menu_open }" class="px-5 py-2 transform hover:bg-gray-400 transition duration-500 hover:scale-100 md:hidden flex">
+                      <a href="/#facility-booking"  class="cursor-pointer text-white font-bold hover:text-black uppercase ">FACILITY BOOKING</a>
+                    </li>
   
                     
             </ul>
